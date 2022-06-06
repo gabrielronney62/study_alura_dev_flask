24809094f8397c58fd1d01e5c0af74b5f6eb34e1
Gabriel Ronney da Silva <gabrielronney62@gmail.com>
2022-06-06 01:30:46 -0300

    Meu primeiro commit.

---
+++ Python3/Exercicios/adivinhacao.py
@@ -0,0 +1,19 @@
+'''
+A ideia do nosso jogo é termos que acertar um número secreto. Quando o programa estiver rodando, teremos que digitar um
+número e o programa dirá se acertamos ou erramos o número, com várias tentativas e níveis.
+'''
+
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+numero_chute = int(input("Digite um número: "))
+
+print("Você digitou ", numero_chute)
+
+if numero_chute == numero_secreto:
+    print("Parabéns, você acertou!")
+else:
+    print("Que pena, você errou!")
