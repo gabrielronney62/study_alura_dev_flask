62012044def6e3ae09ffa4d4d22eb83911f96b58
Gabriel Ronney da Silva <gabrielronney62@gmail.com>
2022-08-28 02:46:08 -0300

    Adicionado break e continue.

--- Python3/Exercicios/adivinhacao.py
+++ Python3/Exercicios/adivinhacao.py
@@ -19,9 +19,13 @@ total_de_tentativas = 3
 # Adicionando mesma condição com laço For
 for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite um número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -29,6 +33,7 @@ for rodada in range (1, total_de_tentativas + 1):
 
     if(acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
