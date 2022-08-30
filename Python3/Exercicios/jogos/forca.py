1c29243bf634b53bd610059831a5cbd7c6131883
Gabriel Ronney da Silva <gabrielronney62@gmail.com>
2022-08-30 03:02:36 -0300

    Iniciando a criação do jogo da forca.

--- Python3/Exercicios/jogos/forca.py
+++ Python3/Exercicios/jogos/forca.py
@@ -4,6 +4,22 @@ def jogar():
     print("Bem vindo ao jogo da forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
 
     print("Fim do jogo!")
     print('#######################')
