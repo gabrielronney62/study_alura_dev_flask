a7509b824f97e5dca9e76b5a42cd9ad3a4e94b4a
Gabriel Ronney da Silva <gabrielronney62@gmail.com>
2022-08-30 01:28:27 -0300

    Adicionado ao final do jogo, qual era o número secreto que não foi adivinhado e a pontuação final do usuário.

--- Python3/Exercicios/adivinhacao.py
+++ Python3/Exercicios/adivinhacao.py
@@ -54,7 +54,9 @@ for rodada in range(1, total_de_tentativas + 1):
         break
     else:
         if(maior):
-            print("Você errou! O seu chute foi maior que o número secreto.")
+            print("O seu chute foi maior que o número secreto.")
+            if(rodada == total_de_tentativas):
+                print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
         pontos_perdidos = abs(numero_secreto - chute)
