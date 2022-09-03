44153fc71462e48e4f2dbae55285de2e99230638
Gabriel Ronney da Silva <gabrielronney62@gmail.com>
2022-09-03 11:45:50 -0300

    Acrescentando a quantidade de erros por tentativas, mostrando palavras acertadas dentro do indice, finalizando com mensagens de acerto ou erro para o fim do jogo.

--- Python3/Exercicios/jogos/forca.py
+++ Python3/Exercicios/jogos/forca.py
@@ -4,23 +4,38 @@ def jogar():
     print("Bem vindo ao jogo da forca!")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
 
-    while(not enforcou and not acertou):
+    print(letras_acertadas)
 
-        chute = input("Qual letra? ")
-        chute = chute.strip()
+    while not enforcou and not acertou:
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+        chute = input("Qual letra?")
+        chute = chute.strip().upper()
 
-        print("Jogando...")
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("Fim do jogo!")
     print('#######################')
