5239d19f2473dcf44aaefdcb214fa08865497479
gabrielronneydasilva <gabriel.r.silva@sesisc.org.br>
2022-10-08 19:04:12 -0300

    Listando jogos e adicionando novo jogo na lista.

--- jogoteca.py
+++ jogoteca.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def ola():
-    lista = ['Tetris', 'Skyrim', 'Crash']
+    lista = ['Tetris', 'Skyrim', 'Crash', 'Mortal Kombat']
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
