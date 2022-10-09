1edd6a763c7d1375356df1454ee08ccf4297e1b6
gabrielronneydasilva <gabriel.r.silva@sesisc.org.br>
2022-10-09 02:14:21 -0300

    Como adicionar conteúdos dinâmicos através do delimitadores do Jinja2, Como pegar dados do servidor, Como mostrar os atributos na view.

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/')
 def ola():
-    lista = ['Tetris', 'Skyrim', 'Crash', 'Mortal Kombat']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
